f4d40140a367b69c269080f79564660624fcf035
Marcos-Bazbih <marcosbaza100@gmail.com>
2021-09-14 17:14:55 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// ******************________1________********************
+// const ARTICLES = [
+//     { title: "article1", writer: "ben", date: "12.5.94" },
+//     { title: "article2", writer: "dan", date: "25.10.06" },
+//     { title: "article3", writer: "avi", date: "06.03.98" },
+//     { title: "article4", writer: "eli", date: "11.6.21" }
+// ];
+
+// ARTICLES.forEach((article) => {
+//     console.log(article);
+// })
+
+// for (const article of ARTICLES) {
+//     console.log(article);
+// }
+// ******************________1________********************
+
+
+// ******************________2________********************
+// const USERS = [
+//     { name: "ben", age: 20, city: "Ashdod" },
+//     { name: "dan", age: 25, city: "Lod" },
+//     { name: "avi", age: 42, city: "Haifa" },
+//     { name: "eli", age: 67, city: "Tel Aviv" }
+// ];
+
+// USERS.forEach((user) => {
+//     for (const key in user) {
+//         console.log(`${key}: ${user[key]}`);
+//     }
+// });
+// ******************________2________********************
+
+
+// ******************________3________********************
+// const BOOKS = [
+//     { title: "book1", writer: "ben", price: 20 },
+//     { title: "book2", writer: "dan", price: 60 },
+//     { title: "book3", writer: "avi", price: 15 },
+//     { title: "book4", writer: "eli", price: 35 }
+// ];
+
+// BOOKS.forEach((book, i) => {
+//     console.log(book);
+// });
+
+// BOOKS.forEach((book, i) => {
+//     console.log(book.writer);
+// });
+// ******************________3________********************
+
+
+// ******************________4________********************
+// const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let sum = 0;
+
+// NUMBERS.forEach((num) => {
+//     sum += num;
+// })
+// console.log(sum);
+
+// for (const num of NUMBERS) {
+//     sum += num;
+// }
+// console.log(sum);
+// ******************________4________********************
